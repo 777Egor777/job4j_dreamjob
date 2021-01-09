bf46c8173f72ee72dfd243c968aca855c1d5544a
Egor <yegeraskin13@gmail.com>
2021-01-09 15:39:11 +0400

    4. candicates.jsp - список кандидатов. [#435319]

---
+++ src/main/java/ru/job4j/dream/model/Candidate.java
@@ -0,0 +1,51 @@
+package ru.job4j.dream.model;
+
+import java.util.Objects;
+
+/**
+ * @author Geraskin Egor
+ * @version 1.0
+ * @since 09.01.2021
+ */
+public final class Candidate {
+    private final int id;
+    private final String name;
+
+    public Candidate(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Candidate setId(int id) {
+        return new Candidate(id, name);
+    }
+
+    public Candidate setName(String name) {
+        return new Candidate(id, name);
+    }
+
+    public static Candidate of(Candidate c) {
+        return new Candidate(c.id, c.name);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Candidate candidate = (Candidate) o;
+        return id == candidate.id && Objects.equals(name, candidate.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
--- src/main/java/ru/job4j/dream/store/Store.java
+++ src/main/java/ru/job4j/dream/store/Store.java
@@ -1,5 +1,6 @@
 package ru.job4j.dream.store;
 
+import ru.job4j.dream.model.Candidate;
 import ru.job4j.dream.model.Post;
 
 import java.util.Collection;
@@ -16,18 +17,26 @@ import java.util.concurrent.ConcurrentHashMap;
 public final class Store {
     private final static Store INSTANCE = new Store();
     private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
+    private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
 
     private Store() {
         posts.put(1, new Post(1, "Junior Java Job"));
         posts.put(2, new Post(2, "Middle Java Job"));
         posts.put(3, new Post(3, "Senior Java Job"));
+        candidates.put(1, new Candidate(1, "Junior Java"));
+        candidates.put(2, new Candidate(2, "Middle Java"));
+        candidates.put(3, new Candidate(3, "Senior Java"));
     }
 
     public static Store instOf() {
         return INSTANCE;
     }
 
-    public Collection<Post> findAll() {
+    public Collection<Post> findAllPosts() {
         return posts.values();
+    }
+
+    public Collection<Candidate> findAllCandidates() {
+        return candidates.values();
     }
 }
