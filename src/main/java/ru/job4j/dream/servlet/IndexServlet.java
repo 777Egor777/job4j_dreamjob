ff7b234bd9be44a628ab7e84ebec699ee3870a94
Egor <yegeraskin13@gmail.com>
2021-01-11 10:37:48 +0400

    0. MVC в Servlet. [#435339]

---
+++ src/main/java/ru/job4j/dream/servlet/IndexServlet.java
@@ -0,0 +1,19 @@
+package ru.job4j.dream.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Geraskin Egor
+ * @version 1.0
+ * @since 11.01.2021
+ */
+public class IndexServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("index.jsp").forward(req, resp);
+    }
+}
